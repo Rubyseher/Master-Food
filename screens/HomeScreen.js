36265718e745ba4da2e00adb04341ee6b8f47a80
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2022-08-25 16:22:33 +0530

    search bar, header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/solid";
+import { UserIcon } from "react-native-heroicons/outline";
+import { TextInput } from 'react-native-web';
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
+    return (
+        <SafeAreaView className='bg-white pt-5 overflow-hidden'>
+            <Text >
+                <View className='flex-row pb-3 items-center mx-4 space-x-2 w-full'>
+                    <Image source={
+                        require('../logo.jpg')
+                    }
+                        className="w-14 h-14 bg-gray-300 p-4 rounded-full"
+                    />
+                    <View >
+                        <Text className='font-bold text-gray-400 text-xs'>
+                            Deliver now
+                        </Text>
+                        <Text className='font-bold text-xl'>
+                            Current Location
+                            <ChevronDownIcon size={25} color='#00ccbb' />
+                        </Text>
+                    </View>
+                    <View className='absolute top-3.5 right-9 '>
+                        <UserIcon size={30} color='#eb7a46' />
+                    </View>
+                </View>
+
+                <View className='flex-row items-center space-x-2 pb-2 mx-4 '>
+                    <View className='flex-row flex-1 space-x-2 bg-slate-200 p-3 w-80'>
+                        <SearchIcon color='gray' size={20} />
+                        <TextInput placeholder="Resturants and cuisines"
+                            keyboardType='default'
+                        />
+                    </View>
+                </View>
+                <View className='absolute right-5 '>
+                    <AdjustmentsIcon size={30} color='#eb7a46' />
+                </View>
+            </Text>
+        </SafeAreaView >
+    )
 }
 
 export default HomeScreen
